d87737a4b7fe721d64670ab31035faa911edf988
Polina Novikova <9104785163@mail.ru>
2021-12-08 16:35:11 +0300

    First edition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,28 @@
 fun main() {
-    println("Hello World!")
+    val discountBorder1 = 1_000_00
+    val discount1 = 100_00
+    val discountBorder2 = 10_000_00
+    val discountBoughtALot = 0.05
+    val discountMusicLover = 0.01
 
+    val previousPurchaseAmount = 1234_00
+    val isMusicLover = true
+    var purchaseValue = 123_00
+
+    val intermediateDiscount : Int
+    if (previousPurchaseAmount > discountBorder1 && previousPurchaseAmount <= discountBorder2){
+        intermediateDiscount = purchaseValue - discount1
+    } else if (previousPurchaseAmount > discountBorder2){
+        intermediateDiscount = (purchaseValue * ( 1 - discountBoughtALot)).toInt()
+    } else intermediateDiscount = purchaseValue
+
+    val finalDiscount : Int
+    if (isMusicLover) {
+        finalDiscount =  (intermediateDiscount * ( 1 - discountMusicLover)).toInt()
+    } else {
+        finalDiscount = intermediateDiscount
+    }
+    println("покупка - " + purchaseValue / 100 +"."+ purchaseValue % 100 + " \u20BD")
+    println("после применения скидки - " + intermediateDiscount / 100 +"."+intermediateDiscount % 100 + " \u20BD")
+    println("после применения 1% скидки - " + finalDiscount / 100 +"."+finalDiscount % 100 + " \u20BD")
 }
